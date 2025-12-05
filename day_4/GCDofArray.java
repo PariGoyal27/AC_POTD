aff91b6eecaf30f5d5e575d1249b3133b05b3a61
Pari-Goyl <goyalpari678@gmail.com>
2025-12-05 10:20:45 +0530

    Added POTDs day-4

---
+++ day_4/GCDofArray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxElement(int[] nums){
+        int max = nums[0];
+        for(int i : nums){
+            if(i > max){
+                max = i;
+            }
+        }
+        return max;
+    }
+    public int minElement(int[] nums){
+        int min = nums[0];
+        for(int i : nums){
+            if(i < min){
+                min = i;
+            }
+        }
+        return min;
+    }
+    public int findGCD(int[] nums) {
+        int n1 = maxElement(nums);
+        int n2 = minElement(nums);
+        while(n2 != 0){
+            int temp = n2;
+            n2 = n1 % n2;
+            n1 = temp;
+        }
+        return n1;
+    }
+}
