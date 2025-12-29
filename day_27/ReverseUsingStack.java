21fd365fb0c69ce4d847f8a8f4c8f53aeafda563
Pari-Goyl <goyalpari678@gmail.com>
2025-12-29 08:23:55 +0530

    Added day-27 27dec

---
+++ day_27/ReverseUsingStack.java
@@ -0,0 +1,19 @@
+package day_27;
+//https://www.geeksforgeeks.org/problems/reverse-a-string-using-stack/1
+
+import java.util.Stack;
+
+public class ReverseUsingStack {
+    public String reverse(String S) {
+        // code here
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < S.length(); i++){
+            st.push(S.charAt(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < S.length(); i++){
+            sb.append(st.pop());
+        }
+        return sb.toString();
+    }
+}
---
+++ day_27/ValidParanthesis.java
@@ -0,0 +1,26 @@
+package day_27;
+//https://leetcode.com/problems/valid-parentheses/description/
+
+import java.util.Stack;
+
+public class ValidParanthesis {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        char[] arr = s.toCharArray();
+        for(char ch : arr){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else if(ch == ')' || ch == '}' || ch == ']'){
+                if(st.isEmpty()) return false;
+                if(
+                ch == ')' && st.peek() != '(' ||
+                ch == '}' && st.peek() != '{' ||
+                ch == ']' && st.peek() != '['
+                ) return false;
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
