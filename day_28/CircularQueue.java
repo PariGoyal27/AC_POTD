b3cf21d0690dee6708e143895ea32e2beb26169c
Pari-Goyl <goyalpari678@gmail.com>
2026-01-01 17:28:00 +0530

    Added POTDs day-28 28dec

---
+++ day_28/CircularQueue.java
@@ -0,0 +1,58 @@
+package day_28;
+//https://leetcode.com/problems/design-circular-queue/
+
+class CircularQueue {
+    int[] arr;
+    int front;
+    int rear;
+    int capacity;
+
+    public CircularQueue(int k) {
+        arr = new int[k];
+        front = -1;
+        rear = -1;
+        capacity = k;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull())
+            return false;
+        if (front == -1)
+            front = 0;
+        rear = (rear + 1) % capacity;
+        arr[rear] = value;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty())
+            return false;
+
+        //If there is a single element left
+        if (front == rear) {
+            front = rear = -1;
+        } else
+            front = (front + 1) % capacity;
+        return true;
+    }
+
+    public int Front() {
+        if (front == -1)
+            return -1;
+        return arr[front];
+    }
+
+    public int Rear() {
+        if (rear == -1)
+            return -1;
+        return arr[rear];
+    }
+
+    public boolean isEmpty() {
+        return front == -1;
+    }
+
+    public boolean isFull() {
+        return (rear + 1) % capacity == front;
+    }
+}
---
+++ day_28/StreamFirstNonRepeating.java
@@ -0,0 +1,23 @@
+package day_28;
+//https://www.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+import java.util.*;
+
+public class StreamFirstNonRepeating {
+    public String firstNonRepeating(String s) {
+        Queue<Character> q = new LinkedList<>();
+        int[] freq = new int[26];
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(freq[ch-'a'] == 0)
+                q.offer(ch);
+            freq[ch-'a']++;
+            while(!q.isEmpty() && freq[q.peek()-'a'] > 1){
+                q.poll();
+            }
+            sb.append(q.isEmpty() ? '#' : q.peek());
+        }
+        return sb.toString();
+    }
+}
